04c6009b87fec0d15081419c3ba6d43f80188df3
sandeepkamra <sandeepkamara@gmail.com>
2026-05-29 15:23:06 +0530

    react js and tailwindcss 4 setup

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+     <h1 className="text-red-500 bg-blue-400">hello everyone!</h1>
+    </>
+  )
+}
+
+export default App
